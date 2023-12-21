d5bf3a5a6a4ef4944952609858b75a8727b24c06
Pontus Horn <pontus.horn@finch3d.com>
2023-12-21 15:06:14 +0100

    Store house mesh/material in resource

--- src/house.rs
+++ src/house.rs
@@ -13,8 +13,12 @@ pub struct HousePlugin;
 
 impl Plugin for HousePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnExit(GameState::Loading), spawn_house)
-            .add_systems(OnExit(GameState::InGame), despawn_house)
+        app.init_resource::<HouseResource>()
+            .add_systems(
+                OnExit(GameState::Loading),
+                (initialize_house_resource, spawn_house).chain(),
+            )
+            .add_systems(OnExit(GameState::InGame), cleanup)
             .add_systems(
                 Update,
                 follow_mouse
@@ -25,28 +29,41 @@ impl Plugin for HousePlugin {
     }
 }
 
-#[derive(Component)]
-struct House;
+#[derive(Resource, Default)]
+struct HouseResource {
+    mesh: Handle<Mesh>,
+    material: Handle<StandardMaterial>,
+}
 
-fn spawn_house(
-    mut commands: Commands,
+fn initialize_house_resource(
+    mut house_resource: ResMut<HouseResource>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let cube_mesh = meshes.add(shape::Cube { size: 0.4 }.into());
-    let material = materials.add(StandardMaterial {
+    house_resource.mesh = meshes.add(shape::Cube { size: 0.4 }.into());
+    house_resource.material = materials.add(StandardMaterial {
         base_color: Color::GOLD,
         ..default()
     });
+}
+
+#[derive(Component)]
+struct House;
+
+impl House {
+    const ROTATION_Y: f32 = PI / -6.;
+}
+
+fn spawn_house(mut commands: Commands, house_resource: Res<HouseResource>) {
     let position = TilePosition::new(0., 0.);
     let translation = position.to_vec3_with_y(0.2);
 
     commands.spawn((
         PbrBundle {
-            mesh: cube_mesh.clone(),
-            material: material.clone(),
+            mesh: house_resource.mesh.clone(),
+            material: house_resource.material.clone(),
             transform: Transform::from_translation(translation)
-                .with_rotation(Quat::from_rotation_y(PI / -6.)),
+                .with_rotation(Quat::from_rotation_y(House::ROTATION_Y)),
             ..default()
         },
         TilePosition::new(0., 0.),
@@ -56,10 +73,17 @@ fn spawn_house(
     ));
 }
 
-fn despawn_house(mut commands: Commands, house_query: Query<Entity, With<House>>) {
+fn cleanup(
+    mut commands: Commands,
+    house_query: Query<Entity, With<House>>,
+    mut house_resource: ResMut<HouseResource>,
+) {
     for house_entity in house_query.iter() {
         commands.entity(house_entity).despawn_recursive();
     }
+
+    house_resource.mesh = Default::default();
+    house_resource.material = Default::default();
 }
 
 fn follow_mouse(
